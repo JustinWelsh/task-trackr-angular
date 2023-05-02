0fac52ac6e8059aa98142b48cda906c9621acfe3
Justin Welsh <justinwelsh33@gmail.com>
2023-05-01 22:44:58 -0500

    OnInit added to component

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  constructor() { }
+
+  // Life-cycle method
+  ngOnInit(): void {
+      // Use this when you want to initialize code.
+      // http request etc..
+
+      
+  }
 }
