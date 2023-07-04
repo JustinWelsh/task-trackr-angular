438fbb99fc95242dda40454a6264ee948a7ad564
Justin Welsh <justinwelsh33@gmail.com>
2023-07-03 20:55:37 -0500

    api and db configured

--- api/src/main/java/com/example/api/models/Task.java
+++ api/src/main/java/com/example/api/models/Task.java
@@ -1,5 +1,35 @@
 package com.example.api.models;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String title;
+    
+    private String description;
+    
+    private LocalDateTime dueDate;
     
 }
